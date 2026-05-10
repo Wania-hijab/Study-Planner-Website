563f8b72ba1cff1422d2fb0f13c0723ca5853bd7
63852-EmanAnsar <63852@students.riphah.edu.pk>
2026-05-11 01:21:07 +0500

    Create script.js

---
+++ Eman-portfolio/script.js
@@ -0,0 +1,14 @@
+// Scroll reveal
+var obs = new IntersectionObserver(function(entries) {
+  entries.forEach(function(e) {
+    if (e.isIntersecting) {
+      e.target.classList.add('visible');
+      obs.unobserve(e.target);
+    }
+  });
+}, { threshold: 0.12 });
+
+document.querySelectorAll('.reveal').forEach(function(el) {
+  obs.observe(el);
+});
+
